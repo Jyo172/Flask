09dd194af3768a4aae7bab36a3e604be916a0edf
Jyoti Prajapati <jyotiprajapati172@gmail.com>
2020-06-16 14:36:05 +0530

    Add files via upload

---
+++ COVID_Pred_flask/home.py
@@ -0,0 +1,25 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/', methods=['GET'])
+def home():
+    if request.method == 'GET':
+        # show the upload form
+        return render_template('home.html')
+
+    if request.method == 'POST':
+        # check if a file was passed into the POST request
+        if 'image' not in request.files:
+            flash('No file was uploaded.')
+            return redirect(request.url)
+
+        image_file = request.files['image']
+
+        # if filename is empty, then assume no upload
+        if image_file.filename == '':
+            flash('No file was uploaded.')
+            return redirect(request.url)
+
+if __name__ == "__main__":
+    app.run('127.0.0.1')
